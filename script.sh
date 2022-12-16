306229033ba84d0b7d19b58dc1de10b6bd6db890
TonyDiaz03 <112600770+TonyDiaz03@users.noreply.github.com>
2022-12-15 20:28:49 -0800

    Add files via upload

--- script.sh
+++ script.sh
@@ -2,25 +2,11 @@
 
 # Loop indefinitely
 while true; do
-    # Prompt user for input
-    read -p "Enter row and column: " row col
+    # Choose next move randomly
+    row=$((RANDOM % 3 + 1))
+    col=$((RANDOM % 3 + 1))
 
-    # Check for specific input
-    if [ "$row" == "q" ] || [ "$col" == "q" ]; then
-        # Quit game if "q" is entered
-        break
-    elif [ "$row" == "1" ] && [ "$col" == "1" ]; then
-        # Make move at row 1, column 1
-        makeMove 1 1
-    elif [ "$row" == "1" ] && [ "$col" == "2" ]; then
-        # Make move at row 1, column 2
-        makeMove 1 2
-    elif [ "$row" == "1" ] && [ "$col" == "3" ]; then
-        # Make move at row 1, column 3
-        makeMove 1 3
-    else
-        # Invalid input
-        echo "Invalid input. Try again."
-    fi
+    # Make move
+    makeMove $row $col
 done
 
